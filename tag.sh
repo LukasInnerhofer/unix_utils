75429f19d989b21afe37ce8d8ee6b8b7ada3ff8d
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-10-07 23:20:43 +0200

    Tags files using hardlinks

---
+++ tag.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+printUsage()
+{
+	echo "Usage:"
+	echo "$0 file tags"
+}
+
+if [ $# -lt 2 ]; then
+	printUsage
+	exit 1
+fi
+
+if [ ! -f $1 ]; then
+	echo "\"$1\" is not a file."
+	printUsage
+	exit 1
+fi
+
+file="$(basename $1)"
+echo "Tagging $1..."
+for tag in "${@:2}"; do
+	if [ -f $tag/$file ]; then
+		echo "Tag \"$tag\" exists. Skipping."
+		continue
+	fi
+	[ ! -d $tag ] && mkdir -p $tag
+	ln $1 $tag/$file
+	echo $tag
+done
+
+echo "Done. Goodbye :^)"
+
