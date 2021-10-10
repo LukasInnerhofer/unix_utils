04e116d653951cb61014e07ac4418ac8e0b98141
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-10-10 21:24:30 +0200

    :2 is not POSIX

--- tag/tag.sh
+++ tag/tag.sh
@@ -11,22 +11,23 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
-if [ ! -f $1 ]; then
+if [ ! -f "$1" ]; then
 	echo "\"$1\" is not a file."
 	printUsage
 	exit 1
 fi
 
-file="$(basename $1)"
+file="$(basename "$1")"
 echo "Tagging $1..."
-for tag in "${@:2}"; do
-	if [ -f $tag/$file ]; then
+for tag in "$@"; do
+	[ "$tag" = "$1" ] && continue
+	if [ -f "$tag/$file" ]; then
 		echo "Tag \"$tag\" exists. Skipping."
 		continue
 	fi
-	[ ! -d $tag ] && mkdir -p $tag
-	ln $1 $tag/$file
-	echo $tag
+	[ ! -d "$tag" ] && mkdir -p "$tag"
+	ln "$1" "$tag/$file"
+	echo "$tag"
 done
 
 echo "Done. Goodbye :^)"
